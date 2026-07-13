f2f05dab44982e7e8eade6267402d2da1b7aa417
Prachi Soni <prachisoni2077@gmail.com>
2026-07-13 20:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (74.27%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-   for (let i = 0; i <= nums.length; i++) {
-        if (!nums.includes(i)) {
-            return i;
-        }
-   }
+  let n=nums.length;
+  let sum=n*(n+1)/2;
+   let summ=0;
+  for(let i=0;i<nums.length;i++){
+    summ=summ+nums[i]
+  }
+   return sum-summ;
 };
