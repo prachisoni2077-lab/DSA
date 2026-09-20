7d2a690c35eccaaeb93954a9d12c3066772cb232
Prachi Soni <249251004+prachisoni2077-lab@users.noreply.github.com>
2026-09-20 12:08:18 +0530

    Time: 2 ms (10.31%) | Memory: 55.5 MB (96.81%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -14,16 +14,19 @@ var mergeTwoLists = function(l1, l2) {
 
     if(!l1) return l2;
     if(!l2) return l1;
-    let curr=null;
-    if(l1.val<l2.val){
-        curr=l1;
-        l1=l1.next;
-    }else{
-        curr=l2;
-        l2=l2.next;
-    }
+    let start= new ListNode();
+    let curr=start;
+    let currr=start;
+    // let curr=null;
+    // if(l1.val<l2.val){
+    //     curr=l1;
+    //     l1=l1.next;
+    // }else{
+    //     curr=l2;
+    //     l2=l2.next;
+    // }
 
-    let start=curr;
+    // let start=curr;
     while(l1 && l2){
         if(l1.val<l2.val){
             curr.next=l1;
@@ -42,5 +45,5 @@ var mergeTwoLists = function(l1, l2) {
     if(!l2){
         curr.next=l1;
     }
-    return start;
+    return currr.next;
 };
