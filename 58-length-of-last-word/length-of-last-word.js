4f9607fcabd1e12dc327808eec0daafded1e6224
Prachi Soni <249251004+prachisoni2077-lab@users.noreply.github.com>
2026-09-26 01:06:33 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (41.30%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,7 +3,25 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s=s.trim();
-    s=s.split(" ");
-    return s[s.length-1].length;
+    let n= s.length-1;
+
+    while(n>=0){
+        if(s[n]==" "){
+            n--;
+        }else{
+            break;
+        }
+    }
+
+    let count=0;
+
+    while(n>=0){
+        if(s[n]!=" "){
+            count++;
+            n--;
+        }else{
+            break;
+        }
+    }
+    return count;
     };
